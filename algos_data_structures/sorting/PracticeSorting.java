bc8eda66d145c21a7e29311129b3305916f87656
Pushkar Modi <pushkar@gigwalk.com>
2013-05-20 12:56:04 -0700

    Initialize sorting

---
+++ algos_data_structures/sorting/PracticeSorting.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class PracticeSorting {
+	public static void main(String[] args) {
+		try {
+			ArrayList<Integer> values = new ArrayList<Integer>(
+				Arrays.asList(84, 54, 85, 59, 31, 3, 63, 99, 86, 87)
+			);
+
+
+		} catch(Exception e) {
+			System.out.println(e.getMessage());
+		} finally {
+			// Do something
+		}
+	}
+}
