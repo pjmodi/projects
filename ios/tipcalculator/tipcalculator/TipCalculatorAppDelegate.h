919846fa73b3cb1a29507b10f06fb2d312b9b7a7
Pushkar Modi <code@pushkarmodi.com>
2013-09-18 15:39:37 -0700

    TipCalculator

---
+++ ios/tipcalculator/tipcalculator/TipCalculatorAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TipCalculatorAppDelegate.h
+//  tipcalculator
+//
+//  Created by pushkar on 9/18/13.
+//  Copyright (c) 2013 pjmodi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TipCalculatorAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ios/tipcalculator/tipcalculator/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  tipcalculator
+//
+//  Created by pushkar on 9/18/13.
+//  Copyright (c) 2013 pjmodi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TipCalculatorAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([TipCalculatorAppDelegate class]));
+	}
+}
