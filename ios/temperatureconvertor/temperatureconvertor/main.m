cdec56fc02640ba759604e092cce787df1d32069
Pushkar Modi <code@pushkarmodi.com>
2013-10-06 19:46:01 -0700

    HW1: Initial blank project

---
+++ ios/temperatureconvertor/temperatureconvertor/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  temperatureconvertor
+//
+//  Created by pushkar on 10/6/13.
+//  Copyright (c) 2013 pjmodi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TemperatureConvertorAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([TemperatureConvertorAppDelegate class]));
+	}
+}
