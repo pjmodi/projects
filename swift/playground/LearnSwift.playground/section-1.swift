c0557a796fe86e86afb4b1861570493a3402f550
Pushkar M <code@pushkarmodi.com>
2015-02-08 03:11:47 -0800

    Revert 96dacaf..8fdfc3f
    This rolls back to commit 96dacaf0cd7f986c4031b053ac6c5064d0c7c85e.

---
+++ swift/playground/LearnSwift.playground/section-1.swift
@@ -0,0 +1,18 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+// Constant
+let maximumLoginAttempts = 10;
+
+// Variables
+var currentLogins = 0;
+currentLogins = 1;
+var x = 0, y = 0.0;
+x
+y = 5;
+var name = "Pushkar";
+var isSet = true;
+isSet = false;
