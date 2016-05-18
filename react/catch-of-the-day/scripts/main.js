b6104269587a1b17e3e3a2cb8d035e220146799f
pushkar <pushkar@turo.com>
2016-05-17 18:38:52 -0700

    Ch. 17 - Hook up linkState for inventory.

--- react/catch-of-the-day/scripts/main.js
+++ react/catch-of-the-day/scripts/main.js
@@ -13,7 +13,11 @@ var h = require('./helpers');
 var Rebase = require('re-base');
 var base = Rebase.createClass('https://glaring-fire-8643.firebaseio.com/');
 
+var Catalyst = require('react-catalyst');
+
 var App = React.createClass({
+    mixins : [Catalyst.LinkedStateMixin],
+
     getInitialState : function() {
         return {
             fishes : {},
@@ -77,7 +81,7 @@ var App = React.createClass({
                     </ul>
                 </div>
                 <Order fishes={this.state.fishes} order={this.state.order} />
-                <Inventory addFish={this.addFish} loadSamples={this.loadSamples} />
+                <Inventory addFish={this.addFish} loadSamples={this.loadSamples} fishes={this.state.fishes}  linkState={this.linkState} />
             </div>
         )
     }
@@ -217,10 +221,31 @@ var Order = React.createClass({
 });
 
 var Inventory = React.createClass({
+    renderInventory : function(key) {
+        var linkState = this.props.linkState;
+
+        return  (
+            <div className="fish-edit" key={key}>
+                <input type="text" valueLink={linkState('fishes.' + key + '.name')} />
+                <input type="text" valueLink={linkState('fishes.' + key + '.price')} />
+                <select valueLink={linkState('fishes.' + key + '.status')} >
+                    <option value="available">Fresh!</option>
+                    <option value="unavailable">Sold Out!</option>
+                </select>
+                <textarea type="text" valueLink={linkState('fishes.' + key + '.desc')}></textarea>
+                <input type="text" valueLink={linkState('fishes.' + key + '.image')} />
+            </div>
+        )
+    },
+
     render: function() {
         return(
             <div>
                 <h2>Inventory</h2>
+
+                {/* Render each fish for editing */}
+                {Object.keys(this.props.fishes).map(this.renderInventory)}
+
                 <AddFishForm {...this.props} />
                 <button onClick={this.props.loadSamples}>Load Sample Fishes</button>
             </div>
