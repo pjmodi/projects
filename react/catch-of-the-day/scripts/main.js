c7b7fc05b59f740996b546a8138026238617dbbd
pushkar <pushkar@turo.com>
2016-05-24 17:29:55 -0700

    Ch. 20 - PropTypes

--- react/catch-of-the-day/scripts/main.js
+++ react/catch-of-the-day/scripts/main.js
@@ -186,6 +186,9 @@ var Header = React.createClass({
                 <h3 className="tagline"><span>{this.props.tagline}</span></h3>
             </header>
         )
+    },
+    propTypes : {
+        tagline : React.PropTypes.string.isRequired
     }
 });
 
@@ -252,6 +255,12 @@ var Order = React.createClass({
                 </CSSTransitionGroup>
             </div>
         )
+    },
+
+    propTypes : {
+        fishes : React.PropTypes.object.isRequired,
+        order : React.PropTypes.object.isRequired,
+        removeFromOrder : React.PropTypes.func.isRequired
     }
 });
 
@@ -286,6 +295,14 @@ var Inventory = React.createClass({
                 <button onClick={this.props.loadSamples}>Load Sample Fishes</button>
             </div>
         )
+    },
+
+    propTypes : {
+        addFish : React.PropTypes.func.isRequired,
+        loadSamples : React.PropTypes.func.isRequired,
+        fishes : React.PropTypes.object.isRequired,
+        linkState : React.PropTypes.func.isRequired,
+        removeFish : React.PropTypes.func.isRequired
     }
 });
 
