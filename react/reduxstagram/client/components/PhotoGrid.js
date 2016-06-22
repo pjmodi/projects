0aa5543001f20f65dd5a4de742bfbaac9e533e1a
pushkar <pushkar@turo.com>
2016-06-21 17:13:56 -0700

    End of 4

---
+++ react/reduxstagram/client/components/PhotoGrid.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+const PhotoGrid = React.createClass({
+	render() {
+		return(
+			<div className="grid-photo">
+				I am the grid 
+			</div>
+		);
+	}
+});
+
+export default PhotoGrid;
